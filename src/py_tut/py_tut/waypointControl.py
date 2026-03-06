afe0f0f5ae7cf7be480fd264a65e6920158851af
alexsroy <alexanderroy956@gmail.com>
2026-03-06 23:17:52 +0000

    Added working waypoint

--- src/py_tut/py_tut/waypointControl.py
+++ src/py_tut/py_tut/waypointControl.py
@@ -1,4 +1,7 @@
-# 90% sure this is untested code.
+# waypointControl Class Written by: Alex Roy
+# Used to create the racecourse by passing commands through PCBComm,
+# update the objective waypoint when you reach the end of the current leg,
+# and tracking the start / end of the race
 
 import rclpy
 import math
@@ -6,53 +9,121 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 from std_msgs.msg import Float32
-
-D = 0.0005
-BBReverseSpace = 2
-
-def calcTriArea(A, B, C):
-    return abs((B[0] * A[1] - A[0] * B[1]) + (C[0] * B[1] - C[1] * B[0]) + (A[0] * C[1] - A[1] * C[0])) / 2
-
-# This line of code may need a magic number tbh to make sure that we are certain the boat is out of the the BB
-def pointInRect(self, point, BB): # https://stackoverflow.com/questions/17136084/checking-if-a-point-is-inside-a-rotated-rectangle
-    return calcTriArea(BB[0], point, BB[3]) + calcTriArea(BB[3], point, BB[2]) + calcTriArea(BB[2], point, BB[1]) + calcTriArea(BB[0], point, BB[1]) <= D * (abs(BB[0][0] - BB[3][0]) / (math.cos(math.radians(self.bearingAngle)) + 0.000000000000000001))
+import json
 
 
-class PCBReadWrite(Node):
-    def __init__(self):
-        super().__init__('WaypointCTRL')
+GATE_WIDTH_METERS = 10
 
-        self.waypointList = {}
 
+class waypointControl(Node):
+    def __init__(self, waypoints):
+        super().__init__('waypointControl')
+
+        self.waypoints = waypoints  # list of (lat, lon)
+        #if there are waypoints set, then create the waypoints from the starting waypoint
+        if len(self.waypoints) > 0:
+            self.origin = self.waypoints[0]
+            self.meter_waypoints = [
+                self._latlon_to_xy(wp) for wp in self.waypoints
+            ]
+        else:
+            self.origin = None
+            self.meter_waypoints = []
+        
+        self.gate_width = GATE_WIDTH_METERS
         self.latitude = 0
         self.longitude = 0
         self.wpTracker = 0
+        
+        
+        #The current leg is the waypoint you are sailing to (0 = going to the start mark)
+        self.max_laps = 1
+        self.current_leg = 0
+        self.lap_count = 0
+        self.following_enabled = False
+        self.race_started = False
+        self.race_complete = False
+        self._was_inside_gate = False
+        
+        
 
+        #Next waypoint coords
         self.waypoint_latitude_publisher = self.create_publisher(Float32, 'waypoint_latitude', 10)
         self.waypoint_longitude_publisher = self.create_publisher(Float32, 'waypoint_longitude', 10)
+        #Previous waypoint coords
         self.prevWaypoint_latitude_publisher = self.create_publisher(Float32, 'previous_waypoint_latitude', 10)
         self.prevWaypoint_longitude_publisher = self.create_publisher(Float32, 'previous_waypoint_longitude', 10)
 
         self.longitude_subscription = self.create_subscription(Float32, 'longitude', self.longitude_callback, 10)
         self.latitude_subscription = self.create_subscription(Float32, 'latitude', self.latitude_callback, 10)
 
-        self.waypointCMD_subscriber = self.create_subscription(String, 'waypoint_command', self.command_callback, 10)
-
+        self.waypointCmd_subscriber = self.create_subscription(String, 'waypoint_command', self.command_callback, 10)
         self.navigationTimer = self.create_timer(0.1, self.waypoint_radius_callback)
 
         self.bearingAngle = 0
 
-
+    #recieve a String with JSON, convert it to a dictionary
     def command_callback(self, msg):
         DICT = json.loads(msg.data)
+        cmd = DICT.get("cmd")
 
-        if DICT["WPcmd"] == "add":
-            # If you mis-type a waypoint, say it's in the 4th position instead of the 3rd position, it will fill both the 3rd and 4th position
-            # with the same waypoint
-            self.waypointList[str(DICT["order"])] = [DICT["longitude"], DICT["latitude"]]
+        if cmd == "add":
+            lat = DICT["latitude"]
+            lon = DICT["longitude"]
 
-        if DICT["WPcmd"] == "remove":
-            self.waypointList.pop(DICT["order"], None)
+            order = DICT.get("order")
+
+            if isinstance(order, int) and 0 <= order <= len(self.waypoints):
+                self.waypoints.insert(order, (lat, lon))
+            else:
+                self.waypoints.append((lat, lon))
+
+            if self.origin is None:
+                self.origin = (lat, lon)
+
+            self.meter_waypoints = [
+                self._latlon_to_xy(wp) for wp in self.waypoints
+            ]
+
+            print(f"Waypoint added: {lat}, {lon}")
+
+        elif cmd == "remove":
+            if not self.waypoints:
+                print("No waypoints to remove.")
+                return
+
+            order = DICT.get("order")
+
+            if isinstance(order, int) and 0 <= order < len(self.waypoints):
+                removed = self.waypoints.pop(order)
+            else:
+                # Default behavior: remove last waypoint
+                removed = self.waypoints.pop()
+
+            print(f"Removed waypoint: {removed}")
+
+            self.meter_waypoints = [
+                self._latlon_to_xy(wp) for wp in self.waypoints
+            ]
+
+        elif cmd == "startFollowing":
+            if not self.waypoints:
+                print("No waypoints to follow.")
+                return
+
+            self.following_enabled = True
+            self.current_leg = 0
+            self.lap_count = 0
+            self.race_started = False
+            self.race_complete = False
+            self._was_inside_gate = False
+
+            print("Following enabled")
+
+        elif cmd == "stopFollowing":
+            self.following_enabled = False
+            print("Following disabled")
+    
 
     def longitude_callback(self, msg):
         self.longitude = msg.data
@@ -62,68 +133,173 @@ class PCBReadWrite(Node):
 
     # Updates the WP tracker
     def waypoint_radius_callback(self):
-        nextWP = self.waypointList[(self.wpTracker + 1) % len(self.wpTracker)] # These are the waypoints we care about in order, we need 2 of them
-        currWP = self.waypointList[(self.wpTracker + 0) % len(self.wpTracker)] # in order to make a BB, so by definition, we need at least 4 WPs
-        prevWP = self.waypointList[(self.wpTracker - 1) % len(self.wpTracker)]
-        doublePrevWP = self.waypointList[(self.wpTracker - 2) % len(self.wpTracker)]
+        if not self.following_enabled:
+            return
 
-        # If in the next BB
-        if self.inBB(currWP, nextWP):
-            self.waypointTracker += 1
+        if len(self.waypoints) == 0:
+            return
 
-        # If in the BB we expect
-        elif self.inBB(prevWP, currWP):
-            pass
+        boat_pos = (self.latitude, self.longitude)
 
-        # If in the previous BB
-        if self.inBB(doublePrevWP, prevWP):
-            self.waypointTracker -= 1
+        triggered = self.update(boat_pos)
 
-        # Publish the lats and lons for the WP
-        f = Float32()
-        f.data(float(self.waypointList[(self.wpTracker + 0) % len(self.wpTracker)][0]))
-        self.waypoint_longitude_publisher.publish(f)
+        if triggered:
+            print("Entered waypoint gate")
 
-        f.data(float(self.waypointList[(self.wpTracker + 0) % len(self.wpTracker)][1]))
-        self.waypoint_latitude_publisher.publish(f)
+        if self.race_complete:
+            return
 
-        f.data(float(self.waypointList[(self.wpTracker - 1) % len(self.wpTracker)][0]))
-        self.prevWaypoint_longitude_publisher.publish(f)
+        # Publish current and previous waypoint
+        target = self.waypoints[self.current_leg]
+        prev_index = (self.current_leg - 1) % len(self.waypoints)
+        prev_wp = self.waypoints[prev_index]
 
-        f.data(float(self.waypointList[(self.wpTracker - 1) % len(self.wpTracker)][1]))
-        self.prevWaypoint_latitude_publisher.publish(f)
+        msg = Float32()
 
-    def inBB(self, prevWP, nextWP):
-        bearingAngle = math.degrees(math.atan2(nextWP[1] - prevWP[1], nextWP[0] - prevWP[0]))
-        bearing = [(prevWP[0] - BBReverseSpace * math.cos(math.radians(bearingAngle)), prevWP[1] - BBReverseSpace * math.sin(math.radians(bearingAngle))), (nextWP[0], nextWP[1])]
+        msg.data = float(target[0])
+        self.waypoint_latitude_publisher.publish(msg)
 
-        APlus = [D / 2 * math.cos(math.radians(bearingAngle + 90)) + bearing[0][0], D / 2 * math.sin(math.radians(bearingAngle + 90)) + bearing[0][1]]
-        AMinus = [D / 2 * math.cos(math.radians(bearingAngle - 90)) + bearing[0][0], D / 2 * math.sin(math.radians(bearingAngle - 90)) + bearing[0][1]]
-        BPlus = [D / 2 * math.cos(math.radians(self.bearingAngle + 90)) + bearing[1][0], D / 2 * math.sin(math.radians(self.bearingAngle + 90)) + bearing[1][1]]
-        BMinus = [D / 2 * math.cos(math.radians(self.bearingAngle - 90)) + bearing[1][0], D / 2 * math.sin(math.radians(self.bearingAngle - 90)) + bearing[1][1]]
+        msg.data = float(target[1])
+        self.waypoint_longitude_publisher.publish(msg)
 
-        boundingBox = [APlus, AMinus, BMinus, BPlus]
+        msg.data = float(prev_wp[0])
+        self.prevWaypoint_latitude_publisher.publish(msg)
 
-        return pointInRect([self.longitude, self.latitude], boundingBox)
+        msg.data = float(prev_wp[1])
+        self.prevWaypoint_longitude_publisher.publish(msg)
+
+
+    def _latlon_to_xy(self, wp):
+        lat, lon = wp
+        ref_lat, ref_lon = self.origin
+
+        R = 6371008  # Earth radius (m)
+
+        dlat = math.radians(lat - ref_lat)
+        dlon = math.radians(lon - ref_lon)
+
+        x = dlon * R * math.cos(math.radians(ref_lat))
+        y = dlat * R
+
+        return (x, y)
+
+    def point_in_gate(self, boat_pos):
+
+        if len(self.meter_waypoints) == 0:
+            return False, 0.0
+
+        boat_xy = self._latlon_to_xy(boat_pos)
+
+        B = self.meter_waypoints[self.current_leg]
+
+        dx = boat_xy[0] - B[0]
+        dy = boat_xy[1] - B[1]
+
+        distance = math.hypot(dx, dy)
+
+        inside = distance <= (self.gate_width / 2.0)
+
+        return inside, distance
+
+
+    # Update race state
+    def update(self, boat_pos):
+
+        if self.race_complete or len(self.waypoints) == 0:
+            return False
+
+        inside, distance = self.point_in_gate(boat_pos)
+        print(f"Leg {self.current_leg} | Distance: {distance:.2f} m | Inside: {inside}")
+        print("Waypoints")
+        print(self.waypoints)
+
+        if inside and not self._was_inside_gate:
+            # PRE-START
+            if not self.race_started and self.current_leg == 0:
+                self.race_started = True
+                self._advance_leg()
+                print("Race started")
+
+            # NORMAL RACING
+            else:
+
+                # If we ENTER waypoint 0 during racing,
+                # that means we finished a lap.
+                if self.race_started and self.current_leg == 0:
+
+                    self.lap_count += 1
+                    print(f"Lap {self.lap_count} complete")
+
+                    if self.max_laps is not None and self.lap_count >= self.max_laps:
+                        self.race_complete = True
+                        print("Race complete")
+                        self._was_inside_gate = True
+                        return True
+
+                self._advance_leg()
+                print(f"Advanced to leg {self.current_leg}")
+
+            self._was_inside_gate = True
+            return True
+
+        # Reset once outside gate
+        if not inside:
+            self._was_inside_gate = False
+
+        return False
+
+
+    def _advance_leg(self):
+        self.current_leg += 1
+        if self.current_leg >= len(self.waypoints):
+            self.current_leg = 0
+
+
+    def current_leg_info(self, boat_pos=None):
+        if self.race_complete:
+            return None
+
+        # PRE-START
+        if not self.race_started:
+            start_wp = self.waypoints[0]
+
+            return "PRESTART", boat_pos, start_wp
+
+        # NORMAL RACING
+        target = self.waypoints[self.current_leg]
+        prev_index = (self.current_leg - 1) % len(self.waypoints)
+        prev_wp = self.waypoints[prev_index]
+
+        return self.current_leg, prev_wp, target
+    
+    def current_target(self):
+        if self.race_complete:
+            return None
+        return self.waypoints[self.current_leg]
+
+
 
 def main(args=None):
     # Main loop
-    #print(getCourse)
+
+    #Test racecourse
+    A = (44.616314, -63.549043)
+    B = (44.619957, -63.548778)
+    C = (44.619894, -63.557008)
+    D = (44.615673, -63.557152)
+
+
+    waypoints = []
 
     rclpy.init(args=args)
 
-    handler = SIM_ROS_HANDLER()
+    handler = waypointControl(waypoints)
+
+    print("Waypoint Control Runner")
 
     rclpy.spin(handler)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
     rclpy.shutdown()
-
-    # Quit Pygame
-    pygame.quit()
 
 
 if __name__ == '__main__':
