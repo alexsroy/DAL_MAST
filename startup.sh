f97bfd9b1408002a195a0bc309207b3fe4e26c23
Andrew Dutka <andrewadutka@gmail.com>
2026-03-13 17:30:07 -0300

    modified startup to use proper x11 formatting

--- startup.sh
+++ startup.sh
@@ -3,4 +3,10 @@
 IMAGE="rredwiz/dalmast:latest"
 
 docker build -t "$IMAGE" .
-docker run -it -v "$(pwd):/workspace" -w /workspace "$IMAGE"
+docker run -it \
+  -v "$(pwd):/workspace" \
+  -w /workspace \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  "$IMAGE"
+
