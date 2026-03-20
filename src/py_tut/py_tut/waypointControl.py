2e27fc561c1166833756e68cdfd3d2a5e965794c
alexsroy <alexanderroy956@gmail.com>
2026-03-20 20:34:03 +0000

    removed unneccessary code

--- src/py_tut/py_tut/waypointControl.py
+++ src/py_tut/py_tut/waypointControl.py
@@ -22,8 +22,6 @@ GATE_WIDTH_METERS = 50
 class waypointControl(Node):
     def __init__(self, waypoints):
         super().__init__('waypointControl')
-        qos = QoSProfile(depth=1)
-        qos.durability = DurabilityPolicy.TRANSIENT_LOCAL
 
         self.waypoints = waypoints  # list of (lat, lon)
         #if there are waypoints set, then create the waypoints from the starting waypoint
@@ -66,8 +64,7 @@ class waypointControl(Node):
         self.waypointCmd_subscriber = self.create_subscription(String, 'waypoint_command', self.command_callback, 10)
         self.navigationTimer = self.create_timer(0.1, self.waypoint_radius_callback)
         
-        #self.waypoint_list_publisher = self.create_publisher(String, 'waypoint_list', 10)
-        self.waypoint_list_publisher = self.create_publisher(String, 'waypoint_list', qos)
+        self.waypoint_list_publisher = self.create_publisher(String, 'waypoint_list', 10)
         self.current_waypoint_index_publisher = self.create_publisher(Int32, 'current_waypoint_index', 10)
         
         # Following waypoint command
